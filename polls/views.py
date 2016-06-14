b35901a3b9875fb41015deea8a5cf5cc830df6cf
Juan Ignacio Avendaño Huergo <juanihu94@gmail.com>
2016-06-15 00:17:39 +0200

    v2

--- polls/views.py
+++ polls/views.py
@@ -1,38 +1,31 @@
-from django.http import HttpResponse,  HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from .models import Question
 from .models import Choice
 """Esto es parte de lo serio"""
 from django.shortcuts import render
+from django.views import generic
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')
-	
-	"""Esto es para devolver una tonteria"""
-	#output = ', '.join([q.question_text for q in latest_question_list])
-	#return HttpResponse(output)
-	
-	"""Aqui va lo serio"""
-	context = {'latest_question_list': latest_question_list}
-	return render(request, 'polls/index.html',context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-	#return HttpResponse("You 're looking at question %s." %question_id)
-    question = Question.objects.get(pk=question_id)
-    return render(request, "polls/detail.html",  {
-        "question":question, 
-    })
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-	#response = "You are looking at the results of questions %s."
-	#return HttpResponse(response % question_id)
-    question = Question.objects.get(pk=question_id)
-    return render(request, "polls/results.html",  {
-        "question":question, 
-    })
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
       
-def vote(request, question_id):
-    question=Question.objects.get(pk=question_id)
+def vote(request, pk):
+    question=Question.objects.get(pk=pk)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError,  Choice.DoesNotExist):
@@ -43,4 +36,4 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse("polls:results", args = (question_id, )))
+        return HttpResponseRedirect(reverse("polls:results", args = (pk, )))
